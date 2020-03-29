e1b0f37f75ff2980d52386f6a24223117c741547
PRATIKSHIRBHATE <pratikshirbhate@gmail.com>
2020-03-29 10:21:20 +0530

    updated programs to include features from scikit-learn v0.22'

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -11,26 +11,30 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
 # Encoding categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 labelencoder = LabelEncoder()
 X[:, 3] = labelencoder.fit_transform(X[:, 3])
-onehotencoder = OneHotEncoder(categorical_features = [3])
-X = onehotencoder.fit_transform(X).toarray()
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+columnTransformer = ColumnTransformer([('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X=np.array(columnTransformer.fit_transform(X),dtype=np.str)
 
 # Avoiding the Dummy Variable Trap
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+# Feature Scaling - optional
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+y_train = sc_y.fit_transform(y_train.reshape(-1, 1))
 
 # Fitting Multiple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
@@ -38,4 +42,7 @@ regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
-y_pred = regressor.predict(X_test)
+y_pred = regressor.predict(X_test.astype(np.float64))
+
+# Inverse transform y_pred
+y_pred = sc_y.inverse_transform(y_pred)[:, [0]] 
